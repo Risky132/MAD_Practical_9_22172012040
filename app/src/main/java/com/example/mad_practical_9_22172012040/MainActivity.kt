e925e2a0fdb872f787bda488d7796aea643ea55d
LENOVO <shivampatel19@gnu.ac.in>
2023-10-31 15:06:57 +0530

    4-09

--- app/src/main/java/com/example/mad_practical_9_22172012040/MainActivity.kt
+++ app/src/main/java/com/example/mad_practical_9_22172012040/MainActivity.kt
@@ -1,28 +1,37 @@
-package com.example.mad_practical_9_22172012040
-
 import android.graphics.drawable.AnimationDrawable
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.widget.ImageView
 
 class MainActivity : AppCompatActivity() {
-    lateinit var alarmAnimation : AnimationDrawable
+
+
+    lateinit  var alarmanimation : AnimationDrawable
+    lateinit var heartanimation : AnimationDrawable
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
-        val alarmimage=findViewById<ImageView>(R.id.alarmimage)
-        alarmimage.setBackgroundResource(R.drawable.alarm_animation_list)
-        alarmAnimation=alarmimage.background as AnimationDrawable
-    }
 
+        setContentView(R.layout.activity_main)
+        val alarmimage:ImageView = findViewById(R.id.imageView)
+        alarmimage.setBackgroundResource(R.drawable.alarm_animation_list)
+        alarmanimation =alarmimage.background as AnimationDrawable
+
+        val heart : ImageView = findViewById(R.id.heartImages)
+        heart.setBackgroundResource(R.drawable.heart_animation)
+        heartanimation = heart.background as AnimationDrawable
+    }
     override fun onWindowFocusChanged(hasFocus: Boolean) {
-        if(hasFocus){
-            alarmAnimation.start()
-        }
-        else
-        {
-            alarmAnimation.stop()
-        }
+
         super.onWindowFocusChanged(hasFocus)
+        if (hasFocus){
+            alarmanimation.start()
+            heartanimation.start()
+        }
+        else{
+            alarmanimation.stop()
+            heartanimation.stop()
+        }
     }
 }
